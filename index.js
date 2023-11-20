9fa035a7c50cbd00e4775a91b9381cfbd24a87f2
Ahmed-Camara <camarahmed123@gmail.com>
2023-11-20 19:03:07 +0100

    Changes made to handle .env file

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>");
 });
 const port = process.env.PORT || 3000;
+console.log(`process.env.PORT : ${process.env.PORT}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
