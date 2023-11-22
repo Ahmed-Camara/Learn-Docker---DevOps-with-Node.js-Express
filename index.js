f4ca422cd2b116b6500c8266da8e765c8317ef34
Ahmed-Camara <camarahmed123@gmail.com>
2023-11-22 18:52:50 +0100

    routes defined for users

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   MONGO_PASSWORD,
 } = require("./config/config");
 const postRouter = require("./routes/postRoute");
+const userRouter = require("./routes/userRoute");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>");
 });
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 console.log(`process.env.PORT : ${process.env.PORT}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
