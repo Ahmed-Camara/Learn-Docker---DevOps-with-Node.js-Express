ad6dfcb5918328a63a70d7ffb512a5300764d8fa
Ahmed-Camara <camarahmed123@gmail.com>
2023-11-22 18:37:20 +0100

    Body parser installed in the project

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const {
   MONGO_USER,
   MONGO_PASSWORD,
 } = require("./config/config");
+const postRouter = require("./routes/postRoute");
+const bodyParser = require("body-parser");
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 const connectWithRetry = () => {
@@ -19,9 +21,11 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>");
 });
+app.use("/api/v1/posts", postRouter);
 const port = process.env.PORT || 3000;
 console.log(`process.env.PORT : ${process.env.PORT}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
